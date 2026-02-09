bf512218f7f0518f89813ca4c0d3504787e08865
DAW <adarte82899@gmail.com>
2026-02-09 18:36:16 +0100

    api

--- agente_IA/app.py
+++ agente_IA/app.py
@@ -53,4 +53,4 @@ async def root():
     }
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("app:app", host="127.0.0.1", port=8080, reload=False)
